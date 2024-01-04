ed721a82c7b10408110cbdda5f4a4d3a114d0f51
INDAbhinandan <129822983+INDAbhinandan@users.noreply.github.com>
2024-01-04 06:23:53 +0000

    const var let

---
+++ abhi/const_var_let.js
@@ -0,0 +1,12 @@
+const myId=10;
+let Name="Abhinandan"
+var Email="salmannisgreat1517.com"
+City="lucknow"
+// myId=5 (id can't assign again with same variable)
+
+Name="RAM"
+Email="abhisakhhu.com"
+City="Gorakhpur"
+ console.log(Name,Email,City);
+/* console.table is use for output will be show in table form */ 
+console.table([myId,Name,Email,City]);
